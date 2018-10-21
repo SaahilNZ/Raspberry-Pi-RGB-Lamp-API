6d55837b561bb5833ffbf06fa472b0b1a4e1e8e8
SaahilNZ <saahilh@outlook.co.nz>
2018-10-21 15:00:32 +1300

    Initial commit

---
+++ Controllers/LampController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LampWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LampController : Controller
+    {
+        // GET api/lamp
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // PUT api/lamp/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+    }
+}
