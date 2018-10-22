b65479704afba99814c9f8298dc1f47dfa118aff
SaahilNZ <saahilh@outlook.co.nz>
2018-10-22 17:56:48 +1300

    Post selected colour to the API

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var colorPicker = new iro.ColorPicker("#colourPicker", {
+var colourPicker = new iro.ColorPicker("#colourPicker", {
     color: { r: 255, g: 0, b: 0 },
     css: {
         "#swatch": {
@@ -6,3 +6,24 @@ var colorPicker = new iro.ColorPicker("#colourPicker", {
         }
     }
 });
+
+function SubmitColour() {
+    var red = colourPicker.color.rgb.r;
+    var green = colourPicker.color.rgb.g;
+    var blue = colourPicker.color.rgb.b;
+
+    $.ajax({
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        url: "api/lamp",
+        data: JSON.stringify({
+            red: red,
+            green: green,
+            blue: blue
+        }),
+        success: null,
+        dataType: 'application/json'
+    });
+}
